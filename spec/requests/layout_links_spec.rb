679d2273c8b84e362f5aea0da7c2faf1b2b3a797
Colin Bayer <cbayer@prattmarketing.com>
2011-08-16 14:46:08 -0500

    finished layout n routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content=>"Home")
+  end
+  it "should have a Contact page at '/'" do
+    get '/contact'
+    response.should have_selector('title', :content=>"Contact")
+  end
+  it "should have an About page at '/'" do
+    get '/about'
+    response.should have_selector('title', :content=>"About")
+  end
+  it "should have a Help page at '/'" do
+    get '/help'
+    response.should have_selector('title', :content=>"Help")
+  end
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
+  end   
+end
